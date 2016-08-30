37938b6b2b85fe3654db2d86b51bcc719cb8a71f
lj <1175259316@qq.com>
2016-08-30 16:32:19 +0800

    gg

--- FunctionTest/FunctionTest/TestVC/AnimationTest/AnimationVC.m
+++ FunctionTest/FunctionTest/TestVC/AnimationTest/AnimationVC.m
@@ -58,6 +58,45 @@
 
 
 
+/** 抛物线运动、旋转、放缩动画组合*/
+- (void)positionWithView:(UIView *)view height:(CGFloat)height {
+    
+    CAKeyframeAnimation *keyframeAnimation=[CAKeyframeAnimation animationWithKeyPath:@"position"];
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, view.center.x, view.center.y);//移动到起始点
+    CGPathAddQuadCurveToPoint(path, NULL, 100, 150, 47.5, height);
+    keyframeAnimation.path = path;
+    CGPathRelease(path);
+    
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    // 缩放倍数
+    animation.fromValue = [NSNumber numberWithFloat:1.0]; // 开始时的倍率
+    animation.toValue = [NSNumber numberWithFloat:0.5]; // 结束时的倍率
+    
+    
+    // 对Y轴进行旋转
+    CABasicAnimation *rotationAni = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    // 设定旋转角度
+    rotationAni.fromValue = [NSNumber numberWithFloat:0.0]; // 起始角度
+    rotationAni.toValue = [NSNumber numberWithFloat:2 * M_PI]; // 终止角度
+    
+    
+    /* 动画组 */
+    CAAnimationGroup *group = [CAAnimationGroup animation];
+    // 动画选项设定
+    group.duration = 0.45;
+    group.repeatCount = 1;
+    group.autoreverses = NO;
+    group.animations = [NSArray arrayWithObjects:keyframeAnimation, animation, rotationAni, nil];
+    
+    [view.layer addAnimation:group forKey:@"KCKeyframeAnimation_Position"];
+    
+}
+
+
+
+
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
